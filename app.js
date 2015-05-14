57903c6b6faa1e678e06300548cc553d2145e141
jonasbl <jonasbl@microsoft.com>
2015-05-14 21:59:30 +0200

    Handle request error

--- app.js
+++ app.js
@@ -2,34 +2,44 @@ var proxyPort = 8001;
 var http = require("http");
 var httpsProxy = require("./httpsProxy")
 
+function processRequest(incomingRequest, response)
+{
+    var req = http.request(incomingRequest.url, function(res) {
+        var data = [];
+    
+        res.on('data', function (chunk) {
+            data.push(chunk);
+        });
+        res.on('end', function() {
+    
+          for(var property in res.headers)
+          {
+            response.setHeader(property, res.headers[property]);        
+          }
+            
+          response.statusCode = res.statusCode;
+          response.write(Buffer.concat(data));            
+          response.end();
+    
+          console.log("[%s][%s] Processed %s.", res.statusCode, incomingRequest.socket.remoteAddress, incomingRequest.url);     
+        });
+    
+    });
+    
+    req.on('error', function(e) {
+      	response.writeHead(500, { 'Content-Type': 'text/html' });
+        response.write('<html><body>Unknown error. Perhaps address is incorrect?</body></html>');
+        response.end();
+    });
+    
+    req.end();      
+}
+
 var server = http.createServer(function(incomingRequest, response) {
-  
-  var req = http.request(incomingRequest.url, function(res) {
-    debugger;
-    console.log('(%s) Requesting: %s ', incomingRequest.socket.remoteAddress, incomingRequest.url);
-     
-    var data = [];
-
-    res.on('data', function (chunk) {
-        data.push(chunk);
-    });
-    res.on('end', function() {
-
-      for(var property in res.headers)
-        response.setHeader(property, res.headers[property]);
-        
-      response.statusCode = res.statusCode;
-      response.write(Buffer.concat(data));
-        
-      response.end();
-       
-    });
-  });
-  
-  req.end();    
+  processRequest(incomingRequest, response);   
 });
 
 httpsProxy.setupHttpsProxy(server);
 server.listen(proxyPort);
-console.log("Server is listening");
+console.log("Server is listening on port %s", proxyPort);
  
