79f8c79d97aeb2c14264c63f0288101a57edb4f8
jonasbl <jonasbl@microsoft.com>
2015-05-15 19:41:33 +0200

    Refactoring

---
+++ httpProxy.js
@@ -0,0 +1,52 @@
+var http = require("http");
+
+function copyHeader(headerName, incomingRequest, outgoingRequest)
+{
+  if (incomingRequest.headers[headerName])
+  {
+    outgoingRequest.setHeader(headerName, incomingRequest.headers[headerName]);
+  }
+}
+
+exports.processHttpRequest = function (incomingRequest, response)
+{
+    var req = http.request(incomingRequest.url, function(res) {
+        var data = [];
+
+        res.on('data', function (chunk) {
+            data.push(chunk);
+        });
+        res.on('end', function() {
+    
+          for(var property in res.headers)
+          {
+            var headerValue = res.headers[property];
+            response.setHeader(property, headerValue);
+          }        
+            
+          response.statusCode = res.statusCode;
+          response.write(Buffer.concat(data));            
+          response.end();
+    
+          console.log("[%s][%s] Processed %s.", res.statusCode, incomingRequest.socket.remoteAddress, incomingRequest.url);     
+        });
+    
+    });
+    
+    req.on('error', function(e) {
+      	response.writeHead(500, { 'Content-Type': 'text/html' });
+        response.write('<html><body>Unknown error. Perhaps address is incorrect?</body></html>');
+        response.end();
+    });
+    
+    copyHeader('cookie', incomingRequest, req);
+    copyHeader('accept', incomingRequest, req);
+    copyHeader('connection', incomingRequest, req);
+    copyHeader('authorization', incomingRequest, req);
+    copyHeader('content-type', incomingRequest, req);
+    copyHeader('content-length', incomingRequest, req);
+    copyHeader('pragma', incomingRequest, req);
+    
+    req.end();      
+}
+
