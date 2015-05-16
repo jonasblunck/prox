45450694c7de1f6b20a2bf57d4d606ab1162d5e3
jonasbl <jonasbl@microsoft.com>
2015-05-16 17:25:10 +0200

    Track request counts

--- userStats.js
+++ userStats.js
@@ -1,6 +1,20 @@
 
+var requestCount = {};
+
+function trackRequestCount(user, address)
+{
+  if (!requestCount[user])
+    requestCount[user] = 0;
+    
+  requestCount[user]++;
+  
+  if (0 == (requestCount[user] % 100))
+  {
+    console.log('User [%s] has made %s requests.', user, requestCount[user]);
+  }  
+}
 
 exports.trackUsage = function(user, address)
 {
-  console.log("User '%s' is using '%s'", user, address);
+  trackRequestCount(user, address);
 }
