eb8689ecdc725d799fd1a68793b64d8f47edc055
jonasbl <jonasbl@microsoft.com>
2015-05-31 19:19:18 +0200

    Serve requests for local resources.

--- httpProxy.js
+++ httpProxy.js
@@ -1,7 +1,34 @@
 var http = require("http");
 var stats = require("./userStats")
+var fs = require('fs');
 
-exports.processHttpRequest = function (incomingRequest, response)
+function IsRequestForLocalResource(incomingRequest)
+{
+    return incomingRequest.url[0] == '/';
+}
+
+function writeResponse(response, statusCode, contentType, content)
+{
+    response.writeHead(statusCode,  { 'Content-Type': contentType });
+    response.write(content);
+    response.end();   
+}
+
+function ProcessLocalRequest(incomingRequest, response)
+{
+    var resource = incomingRequest.url.substr(1); // first char should be a '/' which we don't want to include
+    
+    if (fs.existsSync(resource))
+    {
+        writeResponse(response, 200, 'text/html', fs.readFileSync(resource));
+    }
+    else
+    {
+        writeResponse(response, 404, 'text/html', '<html><body>Cannot find what you are looking for.</body></html>');
+    }    
+}
+
+function ProcessExternalRequest(incomingRequest, response)
 {
     // track request     
     stats.trackUsage(incomingRequest.socket.remoteAddress, incomingRequest.url, new Date());
@@ -19,14 +46,24 @@ exports.processHttpRequest = function (incomingRequest, response)
     });
     
     req.on('error', function(e) {
-      	response.writeHead(500, { 'Content-Type': 'text/html' });
-        response.write('<html><body>Unknown error. Perhaps address is incorrect?</body></html>');
-        response.end();
+        writeResponse(response, 500, 'text/html', '<html><body>Unknown error. Perhaps address is incorrect?</body></html>');
     });
     
     for(var header in incomingRequest.headers)
       req.setHeader(header, incomingRequest.headers[header]);
       
-    req.end();      
+    req.end();         
+}
+
+exports.processHttpRequest = function (incomingRequest, response)
+{
+    if (IsRequestForLocalResource(incomingRequest))
+    {
+        ProcessLocalRequest(incomingRequest, response);
+    } 
+    else   
+    {
+        ProcessExternalRequest(incomingRequest, response);
+    }
 }
 
